259300c7d54179fe98ca3167945c5f0c4041c56e
manhvv <manhvv@evolableasia.vn>
2019-06-11 16:01:09 +0700

    fix

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,47 +1,70 @@
 import React, { Component } from 'react';
-import { View, TextInput, TouchableOpacity, Text, FlatList, ActivityIndicator, ScrollView, Keyboard } from 'react-native';
+import { View, TextInput, TouchableOpacity, Text, FlatList, ActivityIndicator, ScrollView, Keyboard, Share } from 'react-native';
 import SearchCard from '../components/SearchCard';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import axios from 'axios';
+import { Button } from 'react-native-elements';
 
 class SearchScreen extends Component {
 
     constructor(props) {
         super(props);
 
-        this.state ={
+        this.state = {
             loading: false,
-            fail: '',
+            fail: true,
         }
     }
 
+    onShare = async () => {
+        try {
+            const result = await Share.share({
+                message:
+                    'Truy cập link gì gì đó để cài đặt ứng dụng',
+            });
+
+            if (result.action === Share.sharedAction) {
+                if (result.activityType) {
+                    // shared with activity type of result.activityType
+                } else {
+                    // shared
+                }
+            } else if (result.action === Share.dismissedAction) {
+                // dismissed
+            }
+        } catch (error) {
+            alert(error.message);
+        }
+    };
+
     _searchPatient = (text) => {
-        this.setState({ loading: true, fail: '' });
-        if (text.length < 1) return;
+        this.setState({ loading: true, fail: true });
+        if (text.length < 10) return;
         axios(api + 'patients/find-patient-by-id', {
             params: {
                 MaBenhNhan: text
             }
-          }).then(response => {
+        }).then(response => {
             let data = response.data;
-            if  (data.status == 'success') {
-                this.setState({ fail: 'Số bệnh nhân tìm thấy: ' + data.patient.length + ' người', patients: data.patient })
-                
+            if (data.status == 'success') {
+                this.setState({ fail: true, patients: data.patient })
+
             } else {
-                this.setState({ fail: 'Không tìm thấy bệnh nhân nào', patients: []})
+                this.setState({ fail: false, patients: [] })
+                
             }
             this.setState({ loading: false });
-          })
-          .catch(error => {
-            console.log(error)
-          })
+        })
+            .catch(error => {
+                console.log(error)
+            })
     }
 
     _renderPatients = () => {
-       if (!this.state.loading) {
-            return(
+        if (!this.state.loading) {
+            return (
                 <ScrollView keyboardShouldPersistTaps='always'>
-                     <FlatList
+                    <FlatList
                         data={this.state.patients}
                         keyboardShouldPersistTaps='always'
                         keyExtractor={e => e.MaBenhNhan}
@@ -56,28 +79,34 @@ class SearchScreen extends Component {
                     />
                 </ScrollView>
             );
-       }
-       return(
-        <ActivityIndicator size="small" color="#00ff00" />
-       )
+        }
+        return (
+            <ActivityIndicator size="small" color="#00ff00" />
+        )
     }
 
     render() {
-        return(
+        return (
             <View>
-                <View style = {{ flexDirection: 'row', alignItems: 'center', padding: 10, backgroundColor: 'rgba(54, 175, 160, 1)'}}>
-                    <TouchableOpacity onPress={ () => { this.props.navigation.goBack() }}>
-                        <View style = {{ paddingRight: 10}}>
-                            <Icon name = "arrow-left" size={20} color='white'/>
+                <View style={{ flexDirection: 'row', alignItems: 'center', padding: 10, backgroundColor: 'rgba(54, 175, 160, 1)' }}>
+                    <TouchableOpacity onPress={() => { this.props.navigation.goBack() }}>
+                        <View style={{ paddingRight: 10 }}>
+                            <Icon name="arrow-left" size={20} color='white' />
                         </View>
                     </TouchableOpacity>
-                    <TextInput style={{ height: 30, padding: 0, paddingLeft: 5, paddingRight: 5, flex: 1, fontSize: 16, backgroundColor: 'white', marginRight: 20 }} 
-                        onChangeText = {(text) => this._searchPatient(text)}
+                    <TextInput style={{ height: 30, padding: 0, paddingLeft: 5, paddingRight: 5, flex: 1, fontSize: 16, backgroundColor: 'white', marginRight: 20 }}
+                        onChangeText={(text) => this._searchPatient(text)}
                         autoFocus={true}
                         keyboardType='default'
                     />
                 </View>
-                {this.state.fail === '' ? <Text></Text> : <Text style={{ marginLeft: 'auto', marginRight: 'auto' }}>{this.state.fail}</Text>}
+                {this.state.fail === true ? <Text></Text> : <View style={{ marginLeft: 'auto', marginRight: 'auto' }}>
+                    <Text>Không tìm thấy bệnh nhân</Text>
+                    <Button
+                        title="Gửi yêu cầu"
+                        onPress={() => this.onShare()}
+                    />
+                </View>}
                 {this._renderPatients()}
             </View>
         );
